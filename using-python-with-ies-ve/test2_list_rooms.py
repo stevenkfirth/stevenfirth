2ddaa958f2a928a34a3fea76b0c5d53baa17a739
Steven Firth <s.k.firth@lboro.ac.uk>
2022-02-15 05:24:54 +0000

    updates

--- using-python-with-ies-ve/test2_list_rooms.py
+++ using-python-with-ies-ve/test2_list_rooms.py
@@ -5,6 +5,10 @@ project = iesve.VEProject.get_current_project()
 models = project.models
 realmodel = models[0]
 bodies = realmodel.get_bodies(False)
-rooms = [body for body in bodies if body.subtype==iesve.VEBody_subtype.room]
+rooms = [body for body in bodies 
+         if body.subtype==iesve.VEBody_subtype.room]
 for room in rooms:
     print(room.id,room.name)
+
+
+
